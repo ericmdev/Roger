04b50deb4b51770745d6c1afff32a7bbe3d74645
Eric Mugerwa <ericmdev@gmail.com>
2016-02-07 15:30:31 +0000

    Add webapp/client/js/app_modules/hello-world/form-command/index.js

---
+++ webapp/client/js/app_modules/hello-world/form-command/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var stylesheet = require('./style.scss');
+
+/*!
+ * Form Command
+ */
+module.exports = function FormCommand() {
+
+    /*
+        Initialise the form.
+    */
+    this.init = function() {
+        
+    }
+
+};
