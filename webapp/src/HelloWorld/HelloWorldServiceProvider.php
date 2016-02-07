1642a34f659f0e4f029c0303f8a98efbea52af78
Eric Mugerwa <ericmdev@gmail.com>
2016-02-07 15:15:25 +0000

    Add webapp/src/HelloWorld/HelloWorldServiceProvider.php

---
+++ webapp/src/HelloWorld/HelloWorldServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace RogerThat\HelloWorld;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Service Provider for HelloWorld.
+ *
+ * @package rogerthat
+ */
+
+ class HelloWorldServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+        
+    }
+}
