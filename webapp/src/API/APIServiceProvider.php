723dc16aa7d6c1f4702ba112a317af5460670265
Eric Mugerwa <ericmdev@gmail.com>
2016-02-07 15:18:55 +0000

    Add webapp/src/API/APIServiceProvider.php

---
+++ webapp/src/API/APIServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace RogerThat\API;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * Service Provider for API.
+ *
+ * @package rogerthat
+ */
+
+ class APIServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $pimple)
+    {
+    	
+    }
+}
