da16096dd1fd9d26a8be117a3b331c2d0d262a1e
Eric Mugerwa <ericmdev@gmail.com>
2016-02-07 14:11:05 +0000

    Add webapp/test/RogerThatTestCase.php

---
+++ webapp/test/RogerThatTestCase.php
@@ -0,0 +1,21 @@
+<?php
+namespace RogerThat\Test;
+use PHPUnit_Framework_TestCase;
+
+/** 
+ * RogerThat test case.
+ *
+ * @package rogerthat
+ */ 
+abstract class RogerThatTestCase extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    { 
+
+    }
+}
